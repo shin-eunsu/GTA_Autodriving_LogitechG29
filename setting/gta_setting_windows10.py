ca0d18b4bedf88e2996f642c5f3da2dd1be503c0
tmdkfk <dev.eunsu@gmail.com>
2020-06-08 21:06:14 +0900

    gta ene-to-end anaconda setting

---
+++ setting/gta_setting_windows10.py
@@ -0,0 +1,24 @@
+# Create date : 2019.02.21 (Eunsu)
+
+# graphics : NVIDIA GeForce RTX 2080 SUPER
+
+# OS
+# windows10 pro 직접설치
+# cuda 9.0, cudnn 7.6.5
+
+import os
+
+os.system('pip install tensorflow-gpu==1.8.0')
+os.system('pip install matplotlib pandas h5py')
+os.system('pip install opencv-python')
+os.system('pip install opencv-contrib-python')
+os.system('pip install ipywidgets')
+
+os.system('pip install keras==2.1.2')
+os.system('pip install image')
+os.system('pip install keras-tqdm')
+os.system('pip install pygame')
+os.system('pip install jupyterlab')
+
+
+
